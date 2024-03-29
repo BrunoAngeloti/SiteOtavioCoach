a56dfba3c7c54e25b4d80be7cf90fd61ba82471e
BrunoAngeloti <bruno.angeloti@terra.com.br>
2024-03-28 21:14:55 -0300

    feat: changing foto

--- containers/About.js
+++ containers/About.js
@@ -1,32 +1,47 @@
-import { Bar } from '../components/Bar'
-import styles from '../styles/containers/About.module.scss'
-import Image from 'next/image'
+import { Bar } from "../components/Bar";
+import styles from "../styles/containers/About.module.scss";
+import Image from "next/image";
 
-import { returnDates } from '../utils/dateUtils'
+import { returnDates } from "../utils/dateUtils";
 
 export function About(props) {
-
-  const { yearsLiving, yearsOld, yearsMarried, yearsKickboxing } = returnDates()
+  const { yearsLiving, yearsOld, yearsMarried, yearsKickboxing } =
+    returnDates();
 
   return (
     <div ref={props.referencia} className={styles.container}>
-        <Image src="/fotoPai.png" width={720} height={710} alt="Otavio Angeloti Pires"/>
-        <div className={styles.textArea}>
-            <h1>Otavio Angeloti Pires</h1>
-            <p>Graduado em Gestão Comercial - Estácio de Sá</p>
-            <Bar type="1"/>
-            <p>
-                Saber quem sou eu é o que me define!!!<br/>
-                Sou Otavio Angeloti Pires, tenho {yearsOld} anos, nascido em São Paulo, morando em Vitória à {yearsLiving} anos.<br/>
-                Pai de Bruno e Pâmela e casado com Valéria à {yearsMarried} anos.<br/><br/>
-                - Coach Integral Sistêmico formado pela Instituição <strong>FEBRACIS</strong>.<br/>
-                - Analista de Perfil Comportamental <strong>CIS ASSESSMENT</strong>.<br/>
-                - Ministrante <strong>“O Poder da Ação”</strong> Best Seller de Paulo Vieira.<br/>
-                - Atuo a mais de 14 anos em gestão de <strong>equipes e pessoas</strong>
-                - Se você não entende de pessoas, não entende de negócios !!!<br/><br/>
-                Minha maior certeza é... <strong>“Que podemos ser o que acreditamos ser!” </strong>.
-            </p>
-        </div>    
+      <Image
+        src="/fotoPaiNova.png"
+        width={400}
+        height={542}
+        alt="Otavio Angeloti Pires"
+        quality={100}
+      />
+      <div className={styles.textArea}>
+        <h1>Otavio Angeloti Pires</h1>
+        <p>Graduado em Gestão Comercial - Estácio de Sá</p>
+        <Bar type="1" />
+        <p>
+          Saber quem sou eu é o que me define!!!
+          <br />
+          Sou Otavio Angeloti Pires, tenho {yearsOld} anos, nascido em São
+          Paulo, morando em Vitória à {yearsLiving} anos.
+          <br />
+          Pai de Bruno e Pâmela e casado com Valéria à {yearsMarried} anos.
+          <br />
+          <br />- Coach Integral Sistêmico formado pela Instituição{" "}
+          <strong>FEBRACIS</strong>.<br />- Analista de Perfil Comportamental{" "}
+          <strong>CIS ASSESSMENT</strong>.<br />- Ministrante{" "}
+          <strong>“O Poder da Ação”</strong> Best Seller de Paulo Vieira.
+          <br />- Atuo a mais de 14 anos em gestão de{" "}
+          <strong>equipes e pessoas</strong>
+          - Se você não entende de pessoas, não entende de negócios !!!
+          <br />
+          <br />
+          Minha maior certeza é...{" "}
+          <strong>“Que podemos ser o que acreditamos ser!” </strong>.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
--- containers/BlogsContainer.js
+++ containers/BlogsContainer.js
@@ -1,55 +1,71 @@
-import styles from '../styles/containers/Blogs.module.scss'
+import styles from "../styles/containers/Blogs.module.scss";
 
-import { Bar } from '../components/Bar'
-import Image from 'next/image'
-import Link from 'next/link'
-import { useRouter } from 'next/router'
+import { Bar } from "../components/Bar";
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-export function BlogsContainer({ blogs }){
-    if(blogs.length === 0) return(null) 
+export function BlogsContainer({ blogs }) {
+  if (blogs.length === 0) return null;
 
-    const router = useRouter()
-    const total = router.pathname === '/' ? 3 : blogs.length
+  const router = useRouter();
+  const total = router.pathname === "/" ? 3 : blogs.length;
 
-    function handleButton(){
-        // levar para o path /blogs
-        router.push('/blogs')
-    }
+  function handleButton() {
+    // levar para o path /blogs
+    router.push("/blogs");
+  }
 
-    return (
-        <div className={styles.container}>
-            <div className={styles.header}>               
-                <h1>BLOG</h1>  
-                <Bar type="1" color="blue" /> 
-            </div>
-            <div className={styles.content}>
-                {blogs?.map((blog, index) => (
-                    index < total &&
-                    <Link key={index} href={`/blogs/${blog.slugs[0]}`} passHref>
-                        <a className={styles.cardBlog}>
-                            <div className={styles.imageWrapper}>
-                                <Image src={blog.data.imagem_1.url} alt="blog" layout='fill' objectFit='contain'/>
-                            </div>
-                            <div className={styles.contentCard}>
-                                <div className={styles.headerCard}>
-                                    <h1>{blog.data.titulo[0].text}</h1>
-                                    <span>
-                                        {blog.data.data ? blog.data.data.split('-').reverse().join('/') : null}
-                                    </span>
-                                </div>
-                                <p>{blog.data.conteudo[0].text}</p>
-                                <span className={styles.link}>
-                                    <span>
-                                        Veja completo
-                                    </span>
-                                    <Image src="/arrow-right2.svg" alt="seta para direita" width={10} height={10}/>
-                                </span>
-                            </div>
-                        </a>
-                    </Link>
-                ))}
-                {router.pathname === '/' && <button className={styles.button} onClick={handleButton}>Ver todas as postagens</button>}
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>BLOG</h1>
+        <Bar type="1" color="blue" />
+      </div>
+      <div className={styles.content}>
+        {blogs?.map(
+          (blog, index) =>
+            index < total && (
+              <Link key={index} href={`/blogs/${blog.slugs[0]}`} passHref>
+                <a className={styles.cardBlog}>
+                  <div className={styles.imageWrapper}>
+                    <Image
+                      src={blog.data.imagem_1.url}
+                      alt="blog"
+                      layout="fill"
+                      objectFit="cover"
+                    />
+                  </div>
+                  <div className={styles.contentCard}>
+                    <div className={styles.headerCard}>
+                      <h1>{blog.data.titulo[0].text}</h1>
+                      <span>
+                        {blog.data.data
+                          ? blog.data.data.split("-").reverse().join("/")
+                          : null}
+                      </span>
+                    </div>
+                    <p>{blog.data.conteudo[0].text}</p>
+                    <span className={styles.link}>
+                      <span>Veja completo</span>
+                      <Image
+                        src="/arrow-right2.svg"
+                        alt="seta para direita"
+                        width={10}
+                        height={10}
+                      />
+                    </span>
+                  </div>
+                </a>
+              </Link>
+            )
+        )}
+        {router.pathname === "/" && (
+          <button className={styles.button} onClick={handleButton}>
+            Ver todas as postagens
+          </button>
+        )}
+      </div>
+    </div>
+  );
 }
