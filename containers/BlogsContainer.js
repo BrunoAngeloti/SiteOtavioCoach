776076903aafd9e077baf8b4f2bb2099f0e8b4ed
BrunoAngeloti <bruno.angeloti@terra.com.br>
2024-03-31 19:21:02 -0300

    feat: mudando o slug

--- containers/BlogsContainer.js
+++ containers/BlogsContainer.js
@@ -16,6 +16,21 @@ export function BlogsContainer({ blogs }) {
     router.push("/blogs");
   }
 
+  console.log(blogs);
+
+  function formatarParaURL(texto) {
+    // Remove acentos
+    texto = texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    // Substitui espaços por hífens
+    texto = texto.replace(/\s+/g, "-");
+    // Remove caracteres especiais, mantém letras, números e hífens
+    texto = texto.replace(/[^a-zA-Z0-9-]/g, "");
+    // Converte para minúsculas
+    texto = texto.toLowerCase();
+
+    return texto;
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -26,7 +41,11 @@ export function BlogsContainer({ blogs }) {
         {blogs?.map(
           (blog, index) =>
             index < total && (
-              <Link key={index} href={`/blogs/${blog.slugs[0]}`} passHref>
+              <Link
+                key={index}
+                href={`/blogs/${formatarParaURL(blog.data.titulo[0].text)}`}
+                passHref
+              >
                 <a className={styles.cardBlog}>
                   <div className={styles.imageWrapper}>
                     <Image
--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,77 +1,107 @@
-import Image from 'next/image';
-import Prismic from 'prismic-javascript'
-import Link from 'next/link';
+import Image from "next/image";
+import Prismic from "prismic-javascript";
+import Link from "next/link";
 
-import styles from '../../styles/pages/Blog.module.scss'
+import styles from "../../styles/pages/Blog.module.scss";
 
 export default function Blog({ blog }) {
-    console.log(blog)
-    return (
-        <div className={styles.container}>
-            <Link href="/blogs" passHref>
-                <a className={styles.link}>
-                    <Image src="/arrow-left.svg" alt="seta para esquerda" width={15} height={15}/>
-                    <span>Voltar</span>
-                </a>
-            </Link>
-            <div className={styles.content}>
-                <h1>{blog.titulo[0].text}</h1>
-                <span className={styles.data}>{
-                    blog.data.split('-').reverse().join('/')
-                }</span>
-                {blog?.imagem_1?.url && (
-                    <div className={styles.imageWrapper}>
-                        <Image 
-                            src={blog.imagem_1.url} 
-                            alt="imagem do blog"
-                            width={blog.imagem_1.dimensions.width} 
-                            height={blog.imagem_1.dimensions.height}
-                        />
-                    </div>
-                )}
-                <div className={styles.textWrapper}>
-                    {blog.conteudo.map((text, index) => (
-                        <p key={index}>{text.text}</p>
-                    ))}
-                </div>
-                {blog?.imagem_2?.url && (
-                    <div className={styles.imageWrapper}>
-                        <Image 
-                            src={blog.imagem_2.url} 
-                            alt="imagem do blog"
-                            width={blog.imagem_2.dimensions.width} 
-                            height={blog.imagem_2.dimensions.height}
-                        />
-                    </div>
-                )}
-                {blog.link_para_conteudo_externo.url && (
-                <span className={styles.externo}>{
-                    <a href={blog.link_para_conteudo_externo.url} target="_blank" rel="noreferrer">
-                        Veja mais neste link
-                    </a>
-                }</span>
-                )}
-            </div>
+  console.log(blog);
+  return (
+    <div className={styles.container}>
+      <Link href="/blogs" passHref>
+        <a className={styles.link}>
+          <Image
+            src="/arrow-left.svg"
+            alt="seta para esquerda"
+            width={15}
+            height={15}
+          />
+          <span>Voltar</span>
+        </a>
+      </Link>
+      <div className={styles.content}>
+        <h1>{blog.titulo[0].text}</h1>
+        <span className={styles.data}>
+          {blog.data.split("-").reverse().join("/")}
+        </span>
+        {blog?.imagem_1?.url && (
+          <div className={styles.imageWrapper}>
+            <Image
+              src={blog.imagem_1.url}
+              alt="imagem do blog"
+              width={blog.imagem_1.dimensions.width}
+              height={blog.imagem_1.dimensions.height}
+            />
+          </div>
+        )}
+        <div className={styles.textWrapper}>
+          {blog.conteudo.map((text, index) => (
+            <p key={index}>{text.text}</p>
+          ))}
         </div>
-    )
+        {blog?.imagem_2?.url && (
+          <div className={styles.imageWrapper}>
+            <Image
+              src={blog.imagem_2.url}
+              alt="imagem do blog"
+              width={blog.imagem_2.dimensions.width}
+              height={blog.imagem_2.dimensions.height}
+            />
+          </div>
+        )}
+        {blog.link_para_conteudo_externo.url && (
+          <span className={styles.externo}>
+            {
+              <a
+                href={blog.link_para_conteudo_externo.url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Veja mais neste link
+              </a>
+            }
+          </span>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function formatarParaURL(texto) {
+  // Remove acentos
+  texto = texto.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+  // Substitui espaços por hífens
+  texto = texto.replace(/\s+/g, "-");
+  // Remove caracteres especiais, mantém letras, números e hífens
+  texto = texto.replace(/[^a-zA-Z0-9-]/g, "");
+  // Converte para minúsculas
+  texto = texto.toLowerCase();
+
+  return texto;
 }
 
 export const getServerSideProps = async (context) => {
-    try {
+  try {
     const slug = context.query.slug;
-    const client = await Prismic.client('https://angeloticoach.prismic.io/api/v2')
-    const blog = await client.query(Prismic.Predicates.at('document.type', 'blog'))
+    const client = await Prismic.client(
+      "https://angeloticoach.prismic.io/api/v2"
+    );
+    const blog = await client.query(
+      Prismic.Predicates.at("document.type", "blog")
+    );
 
-    const blogFilter = blog.results.filter((blog) => blog.slugs[0] === slug)
+    const blogFilter = blog.results.filter(
+      (blog) => formatarParaURL(blog.data.titulo[0].text) === slug
+    );
 
     return {
       props: {
-        blog: blogFilter[0].data
+        blog: blogFilter[0].data,
       },
     };
-    } catch (error) {
-        return {
-            notFound: true
-        }
-    }
-  };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
+};
